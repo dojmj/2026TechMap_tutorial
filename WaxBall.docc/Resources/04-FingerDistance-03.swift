1225e937c74b6c387eb1a770035a6d8ec265545f
Seoyeon94 <20221022@sungshin.ac.kr>
2026-08-18 23:16:56 +0900

    수정1

---
+++ WaxBall.docc/Resources/04-FingerDistance-03.swift
@@ -0,0 +1,21 @@
+//
+//  04-FingerDistance-03.swift
+//
+
+func printFingerDistances(
+    _ distances: FingerDistances
+) {
+    
+    print("""
+    
+    ===== Finger Distances =====
+    
+    Thumb : \(distances.thumb)
+    Index : \(distances.index)
+    Middle: \(distances.middle)
+    Ring  : \(distances.ring)
+    Little: \(distances.little)
+    
+    ============================
+    """)
+}
